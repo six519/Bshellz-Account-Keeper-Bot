8663f30535b824291b0765ea86616d5dab9652c5
Ferdinand E. Silva <ferdie_v143@yahoo.com>
2012-04-28 18:55:34 +0800

    Add log file and fix bug

--- bshellz_keeper.py
+++ bshellz_keeper.py
@@ -1,4 +1,4 @@
-import socket, re
+import socket, re, os, datetime
 
 IRC_SERVER = "irc.freenode.net"
 IRC_NICK = "bshellz_six519"
@@ -9,39 +9,58 @@ BSHELLZ_USER = "six519"
 def sendMessage(sock, message):
 	sock.send(message.encode())
 
+def getLogFile():
+	now = datetime.datetime.now()
+	return now.strftime("%Y_%m_%d")
+
+def getPath():
+	return os.path.realpath(__file__).replace(__file__, "")
+	
+def createLogFile():
+	logFile = open("%s%s" % (getPath(), getLogFile()), 'w')
+	logFile.write("%s%s" % (getPath(), getLogFile()))
+	logFile.close()
+
 def main():
-	ircSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	quitting = False
 	
-	try:
-		ircSocket.connect((IRC_SERVER, IRC_PORT))
-		print "Connecting to: %s" % IRC_SERVER
-		while True:
-			buffer = ircSocket.recv(1024).decode("utf-8", 'replace')
-			
-			if re.search("Checking Ident", buffer):
-				print "Authenticating your nickname: %s" % IRC_NICK
-				sendMessage(ircSocket, "NICK %s\r\n" % IRC_NICK)
-				sendMessage(ircSocket, "USER %s \"%s.ph\" \"%s\" :%s bobot\r\n" % (IRC_NICK, IRC_NICK, IRC_SERVER, IRC_NICK))
-			elif re.search("Nickname is already in use|Erroneous Nickname|This nickname is registered", buffer):
-				print "There's a problem on your nickname: %s" % IRC_NICK
-				print "\nKeeper quits"
-				quitting = True
-			elif re.search("End of /MOTD command", buffer):
-				print "Keeping your account: %s" % BSHELLZ_USER
-				sendMessage(ircSocket, "JOIN #%s\r\n" % IRC_ROOM)
-				sendMessage(ircSocket, "PRIVMSG #%s :!keep %s\r\n" % (IRC_ROOM, BSHELLZ_USER))
-				sendMessage(ircSocket, "PRIVMSG #%s :Good day to all of you... :)\r\n" % IRC_ROOM)
-				quitting = True
-				
-			if quitting:
-				ircSocket = None
-				break
-		print "Successfully called keep command on the server\n\nKeeper quits"
+	if not os.path.exists("%s%s" % (getPath(), getLogFile())):
+	
+		ircSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		quitting = False
 		
-	except Exception as err:
-		print "An error occurred: %s" % err
-	
+		try:
+			ircSocket.connect((IRC_SERVER, IRC_PORT))
+			print "Connecting to: %s" % IRC_SERVER
+			while True:
+				buffer = ircSocket.recv(1024).decode("utf-8", 'replace')
+				
+				if re.search("Checking Ident", buffer):
+					print "Authenticating your nickname: %s" % IRC_NICK
+					sendMessage(ircSocket, "NICK %s\r\n" % IRC_NICK)
+					sendMessage(ircSocket, "USER %s \"%s.ph\" \"%s\" :%s bobot\r\n" % (IRC_NICK, IRC_NICK, IRC_SERVER, IRC_NICK))
+				elif re.search("Nickname is already in use|Erroneous Nickname|This nickname is registered", buffer):
+					print "There's a problem on your nickname: %s" % IRC_NICK
+					print "\nKeeper quits"
+					quitting = True
+				elif re.search("End of /MOTD command", buffer):
+					print "Joining to channel: %s" % IRC_ROOM
+					sendMessage(ircSocket, "JOIN #%s\r\n" % IRC_ROOM)
+				elif re.search("End of /NAMES list", buffer):
+					print "Keeping your account: %s" % BSHELLZ_USER
+					sendMessage(ircSocket, "PRIVMSG #%s :!keep %s\r\n" % (IRC_ROOM, BSHELLZ_USER))
+					sendMessage(ircSocket, "PRIVMSG #%s :Good day to all of you... :)\r\n" % IRC_ROOM)
+					createLogFile()
+					quitting = True
+					
+				if quitting:
+					ircSocket = None
+					break
+			print "Successfully called keep command on the server\n\nKeeper quits"
+			
+		except Exception as err:
+			print "An error occurred: %s" % err
+	else:
+		print "Keeper quits"
 if __name__ == "__main__":
 	try:
 		main()
